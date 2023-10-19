c2951853603ab5ff49f1a41db0bb619ea077000f
Juan Tellez <juan.f.tellez@gmail.com>
2023-10-19 07:35:14 -0700

    finished week 4

---
+++ files/example-scripts/05-summarize.R
@@ -0,0 +1,18 @@
+# libraries
+library(tidyverse)
+library(juanr)
+library(fivethirtyeight)
+
+# trade data
+bob_ross
+?bob_ross
+
+# do your work here!
+
+
+# load data
+flying
+?flying
+
+
+# do your work here!
