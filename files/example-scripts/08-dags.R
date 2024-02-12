26beba16dba326c6b60a90fbd1f3993edeaaa0b4
Juan Tellez <juan.f.tellez@gmail.com>
2024-02-12 13:39:27 -0800

    what happened here?

--- files/example-scripts/08-dags.R
+++ files/example-scripts/08-dags.R
@@ -1,6 +1,21 @@
 # libraries
 library(tidyverse)
 library(ggdag)
+library(huxtable)
+library(broom)
 
 
-# make dag here
+
+
+## the fork simulation
+fake = tibble(Z = sample(c(0, 1), size = 50, replace = TRUE), 
+              X = rnorm(n = 50, mean = 20, sd = 4) + 10 * Z,
+              Y = rnorm(n = 50, mean = 20, sd = 2) + 8 * Z) 
+
+
+
+# the pipe
+
+
+
+# the collider
---
+++ files/example-scripts/09-uncertainty.R
@@ -0,0 +1,13 @@
+# libraries
+library(tidyverse)
+library(stevedata)
+library(moderndive)
+
+# data
+gss_abortion
+?gss_abortion
+
+
+
+## death penalty
+issues = read_csv("https://www.dropbox.com/s/x5xncajqsz0q09l/voter-files-issues.csv?dl=1")
