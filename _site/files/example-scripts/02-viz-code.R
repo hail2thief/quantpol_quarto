17922fe28c6180af8c3337339c049d722bffed19
Juan Tellez <juan.f.tellez@gmail.com>
2024-09-30 12:15:17 -0700

    added site, which is needed to deploy from github

---
+++ _site/files/example-scripts/02-viz-code.R
@@ -0,0 +1,51 @@
+# load libraries
+library(tidyverse)
+library(gapminder) # install this if you don't have it!
+library(socviz) # install this if you don't have it!
+library(ggthemes) # install this if you don't have it!
+library(tvthemes) # install this if you don't have it!
+
+
+
+# presidential elections ---------------------------------------------------------
+
+
+# look at the data
+elections_historic
+?elections_historic
+
+
+# MAKE THE PLOT HERE!!
+
+
+# recessions -------------------------------------------------------------------
+
+
+# look at econ data
+economics
+?economics # look at variable names
+
+
+# MAKE PLOT HERE
+
+
+
+
+# organs -------------------------------------------------------------------
+
+# organ data: grouped histogram, MAKE PLOT HERE
+organdata
+?organdata
+
+
+
+
+# Code challenge ----------------------------------------------------------
+
+
+# datasets you can use
+starwars
+gapminder
+elections_historic
+organdata
+storms
