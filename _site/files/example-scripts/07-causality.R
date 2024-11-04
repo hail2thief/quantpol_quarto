0d7bdfbb30825054f1708a6d3e804d4aeb6e5981
Juan Tellez <juan.f.tellez@gmail.com>
2024-11-04 07:54:48 -0800

    new slides

--- _site/files/example-scripts/07-causality.R
+++ _site/files/example-scripts/07-causality.R
@@ -11,11 +11,3 @@ fake_election = tibble(party_share = rnorm(n = 500, mean = 50, sd = 5),
 fake_election
 
 
-
-
-# confounding example
-waffle_divorce = tibble(south = sample(c(0, 1), size = 50, replace = TRUE), 
-              waffle = rnorm(n = 50, mean = 20, sd = 4) + 10 * south,
-              divorce = rnorm(n = 50, mean = 20, sd = 2) + 8 * south)
-
-waffle_divorce
